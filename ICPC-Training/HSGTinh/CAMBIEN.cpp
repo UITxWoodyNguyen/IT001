4f8b23348eeac44d6e83cb488211107e6cf9f802
UITxWoodyNguyen <woodydadrillers0707@gmail.com>
2026-01-23 14:26:03 +0700

    Change File

--- ICPC-Training/HSGTinh/CAMBIEN.cpp
+++ ICPC-Training/HSGTinh/CAMBIEN.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 const string FILE_NAME = "test";
-const int MAXN = 1e6 + 36;
-
-long long n, a[MAXN];
+const int MAXN = 1e5 + 36;
+int n,a[MAXN];
 
 int main () {
     ios_base::sync_with_stdio(0);
@@ -17,9 +16,14 @@ int main () {
 
     cin >> n;
     for(int i = 1; i <= n; i++) cin >> a[i];
-    sort(a+1, a+n+1);
+
     int res = 0;
-    for(int i = 1; i <= n; i++) if (i == a[i]) res++;
+    unordered_map<int,int> countSum; // countSum[x] = number of pairs (i,j) such that a[i] + a[j] = x
+    for(int k = 3; k < n; k++) {
+        int j = k-1; // j is the right index of the pair (i,j)
+        for(int i = 1; i < j; i++) countSum[a[i] + a[j]]++; // add pairs (i,j) to the map
+        for(int p = k + 1; p <= n; p++) res += countSum[a[p] - a[k]]; // find pairs (i,j) such that a[i] + a[j] = a[p] - a[k]
+    }
+
     cout << res;
-    return 0;
 }
