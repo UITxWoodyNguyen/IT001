60d00557569b226833bfa6fdebf6cc7783f5e827
UITxWoodyNguyen <woodydadrillers0707@gmail.com>
2026-01-28 14:14:37 +0700

    pub

--- ICPC-Training/MarisaOJ/searching-p2/COMPILE.cpp
+++ ICPC-Training/MarisaOJ/searching-p2/COMPILE.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 const string FILE_NAME = "test";
 const int MAXN = 1e5 + 36;
-int n;
-long long k, a[MAXN], Area[MAXN];
+int n,k;
+struct Element {
+    long long weight, value;
+} a[MAXN];
 
 template <typename T> T getMid (T a, T b, T base) { return ((a + b) / base); }
 
@@ -18,14 +20,17 @@ void file_processing (string filename) {
 }
 
 const int RANGE = 100;
-bool check (long double x) { // check if we can have at least k pieces with area = x
-    if (x <= 0) return true;
-    long long cnt = 0;
+bool check (long double mid) { // check if we can have average >= mid
+    vector<long double> f;
     for(int i = 1; i <= n; i++) {
-        cnt += Area[i] / x;
-        if (cnt >= k) return true;
+        long double cur = (long double) a[i].value - mid * (long double) a[i].weight;
+        f.push_back(cur);
     }
-    return cnt >= k;
+
+    long double sum = 0;
+    sort(f.begin(), f.end(), greater<long double>());
+    for(int i = 0; i < k; i++) sum += f[i];
+    return sum >= 0;
 }
 
 int main () {
@@ -35,21 +40,18 @@ int main () {
     file_processing (FILE_NAME);
 
     cin >> n >> k;
-    long long maxArea = 0;
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-        Area[i] = (long long) (a[i] * a[i]);
-        maxArea = max(maxArea, Area[i]);
-    }
+    for(int i = 1; i <= n; i++) cin >> a[i].value >> a[i].weight;
 
-    long double left = 0, right = maxArea, ans = 0.0;
-    for(int time = 0; time < RANGE; time++) {
+    long double ans = 0;
+    long double left = 0, right = 1e9;
+    for(int i = 0; i < RANGE; i++) {
         long double mid = getMid(left, right, (long double) 2);
-        // cout << mid << endl;
-        if (check(mid)) left = mid;
-        else right = mid;
+        if (check(mid)) {
+            ans = mid;
+            left = mid;
+        } else right = mid;
     }
 
-    cout << fixed << setprecision(3) << left << endl;
+    cout << fixed << setprecision(10) << ans << endl;
     return 0;
 }
--- ICPC-Training/MarisaOJ/searching-p2/README.md
+++ ICPC-Training/MarisaOJ/searching-p2/README.md
@@ -118,3 +118,26 @@
     - Tổng số phần bánh có kích cỡ $x$ có thể cắt được từ $n$ chiếc bánh: $Total = \sum_{i=1}^{n} \lfloor \frac{A_i^2}{x} \rfloor$.
 - Source code: [CAKE.cpp]()
 
+## Sắp xếp hiệu 
+- Tóm tắt đề: Cho mảng $A$ gồm $n$ phần tử. Có tất cả $\frac{n(n-1)}{2}$ cặp giá trị tuyệt đối $|A_i - A_j|$. Cần tìm giá trị nhỏ thứ $k$ trong tất cả các hiệu này sau khi đã sắp xếp không giảm.
+- Giới hạn: $1 \le n \le 10^5$, $1 \le k \le \frac{n(n-1)}{2}$, $1 \le A_i \le 10^9$
+- Solution:
+
+    - Ta cần xác định giá trị $X$ sao cho có ít nhất $k$ cặp $(i, j)$ sao cho $|A_i - A_j| \le X$.
+    - Thực hiện sắp xếp mảng $A$ tăng dần. Ta có nhận xét, khi mảng đã được sắp xếp tăng dần $|A_i - A_j| \le X \Rightarrow A_j - A_i \le X \Rightarrow A_j \le A_i + X$. Từ đó ta cần tìm số giá trị $j$ tương ứng với mỗi vị trí $i$. (Dùng **2 pointers**)
+    - Binary search để tìm giá trị $X$ thoả mãn.
+- Source code: [SUB.cpp]()
+
+## Thu thập
+- Tóm tắt đề: Cho $n$ món đồ, mỗi món có giá trị $v_i$ và cân nặng $w_i$. Cần chọn đúng $k$ món sao cho tỉ số giữa tổng giá trị và tổng cân nặng là lớn nhất: $\frac{\sum v_{selected}}{\sum w_{selected}} \to \max$.
+- Giới hạn:
+
+    - $n, k$ ($1 \le k \le n \le 10^5$).
+    - $n$ cặp $(v_i, w_i)$ ($1 \le v_i, w_i \le 10^9$).
+- Solution: 
+
+    - Giả sử tỉ số tối ưu là $X$. Ta cần tìm $X$ lớn nhất sao cho tồn tại một cách chọn $k$ món thỏa mãn:
+        $$\frac{\sum v_i}{\sum w_i} \ge X \iff \sum v_i \ge X \cdot \sum w_i \iff \sum (v_i - X \cdot w_i) \ge 0$$
+    - Với mỗi giá trị $X$ cố định, ta tính giá trị mới $f_i = v_i - X \cdot w_i$ cho từng món đồ. Để tổng $k$ món lớn nhất, ta chỉ cần chọn $k$ món có $f_i$ lớn nhất. Nếu tổng này $\ge 0$, nghĩa là tỉ số $X$ có thể đạt được hoặc cao hơn.
+- Source code: [DIV.cpp]()
+
